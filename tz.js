211d1cfcd39d17f14094e7d278d8ed28b3498feb
MatildaDelacourt <matilda.delacourt@gmail.com>
2021-10-17 23:03:43 -0400

    edit to yargs

--- tz.js
+++ tz.js
@@ -1,17 +1,18 @@
 const moment = require('moment-timezone');
+const yargs = require('yargs');
 
 moment.tz.setDefault('America/New_York');
 
-let targetTimezone;
+
+
+let targetTimezone = yargs.argv;
 
 if (process.argv.length != 3){
     console.log("Usage: node <script-file> <timezone>");
     process.exit(1);
 }
 else {
-    targetTimezone = process.argv[2];
+    targetTimezone = yargs.argv._[0];
 }
 
-
-console.log(`The time at the ${targetTimezone} timezone is ${moment().tz(targetTimezone).format()}`);
-
+console.log(`The time at the ${targetTimezone} timezone is ${moment().tz(targetTimezone).format('dddd, MMMM Do YYYY, h:mm:ss a')}`);
